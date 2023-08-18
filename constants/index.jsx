f778913dc9cb6f36b127852ebe26565e46385d37
VickySam1901 <72963230+VickySam1901@users.noreply.github.com>
2023-08-18 21:06:19 +0530

    overlay

---
+++ constants/index.jsx
@@ -0,0 +1,9 @@
+
+export const landingPageLiterals = {
+    heading: `Welcome to Your Creative Portfolio Journey`,
+    subHeading: `our portfolio builder platform is designed to empower you to create a 
+    stunning online presence that truly reflects your creativity, skills, and accomplishments. 
+    Whether you're an artist, designer, developer, writer, or any kind of creative professional, 
+    we're here to help you tell your story in a captivating and memorable way.`
+}
+
